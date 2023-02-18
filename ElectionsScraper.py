3b5c506fd79f1004675655d04844ab509bb6477c
laptop\Michaela <hronova.michaela09@gmail.com>
2023-02-18 16:03:45 +0100

    Instalace knihoven, requirements, gitignore

---
+++ ElectionsScraper.py
@@ -0,0 +1,11 @@
+"""
+projekt_3.py: treti projekt do Engeto Online Python Akademie
+author: Michaela Hronová
+email: hronova.michaela09@gmail.com; hronova@ceps.cz
+discord: Míša H.#5316
+"""
+
+import requests
+import bs4
+
+
