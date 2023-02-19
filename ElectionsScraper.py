ec9179d7ebb74f2774482ab5cbb94a130eaf8a49
laptop\Michaela <hronova.michaela09@gmail.com>
2023-02-19 19:50:58 +0100

    funkce zpracovani rozcestniku okresu

--- ElectionsScraper.py
+++ ElectionsScraper.py
@@ -8,7 +8,7 @@ discord: Míša H.#5316
 import requests
 from bs4 import BeautifulSoup
 import unicodedata
-from typing import Dict
+from typing import Dict, List
 
 
 def extract_number_from_cell(cell) -> int:
@@ -41,11 +41,26 @@ def extract_party_table(table_parties) -> Dict[str,int]:
     return party_dict
 
 
-def extract_result_page(parsed_text) -> Dict[str, int]:
+def extract_result_page(parsed_text: BeautifulSoup) -> Dict[str, int]:
     tables = parsed_text.find_all("table")
     overall_dict = extract_overall_data(tables[0])
     party_votes_dict = {}
-    for table in tables_parties:
+    for table in tables[1:]:
         party_votes_dict.update(extract_party_table(table_parties=table))
     overall_dict.update(party_votes_dict)
     return overall_dict    
+
+
+def parse_region_page(parsed_text: BeautifulSoup) -> List[Dict[str, str]]:
+    tables = parsed_text.find_all("table")
+    counties_list = []
+    for table in tables:
+        rows = table.find_all("tr")
+        for row in rows[2:]:
+            county_dict = {}
+            cells = row.find_all("td")
+            county_dict["code"] = cells[0].text
+            county_dict["location"] = cells[1].text
+            county_dict["url"] = cells[2].a["href"]
+            counties_list.append(county_dict)
+    return counties_list   
